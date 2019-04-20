280e8ce1238b6c94473671c96f401c824fd378dc
gustavoaagl <unknown>
2019-04-20 20:27:14 -0300

    Segunda versão full.

---
+++ VersionSQL/W16SQL17/SQLEXPRESS/AdventureWorks/dbo/Table/teste2.sql
@@ -0,0 +1,8 @@
+/****** Object:  Table [dbo].[teste2]    Committed by VersionSQL https://www.versionsql.com ******/
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+CREATE TABLE [dbo].[teste2](
+	[a1] [int] NULL
+) ON [PRIMARY]
+
