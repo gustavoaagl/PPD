a6750895957719b7d151881894f1e6afebcc6c24
gustavoaagl <unknown>
2019-04-20 20:18:08 -0300

    Criação Tabela dbo.teste

---
+++ VersionSQL/W16SQL17/SQLEXPRESS/AdventureWorks/dbo/Table/teste.sql
@@ -0,0 +1,8 @@
+/****** Object:  Table [dbo].[teste]    Committed by VersionSQL https://www.versionsql.com ******/
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+CREATE TABLE [dbo].[teste](
+	[a1] [int] NULL
+) ON [PRIMARY]
+
